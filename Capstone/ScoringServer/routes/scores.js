8a835fed7379ebf5d57598cad624b493af5dda93
nnandiha <nayan.nandihalli@intel.com>
2016-05-11 15:20:20 +0200

    Revved classID to 3 and now display top 5 students

--- Capstone/ScoringServer/routes/scores.js
+++ Capstone/ScoringServer/routes/scores.js
@@ -4,14 +4,14 @@ var mongoose = require('mongoose');
 
 var Score = mongoose.model('Score');
 
-var classID = 2;
+var classID = 3;
 
 var allowedIPs = ['::ffff:127.0.0.1', '::ffff:10.54.32.149'];
 
 router.get('/:limit', function(req, res, next){
 	var limit = Number(req.params.limit);
-	if(limit > 3 && allowedIPs.indexOf(req.connection.remoteAddress) < 0)
-		limit = 3;
+	if(limit > 5 && allowedIPs.indexOf(req.connection.remoteAddress) < 0)
+		limit = 5;
   Score.find({'classID':classID}).sort([['points',-1], ['timestamp',1]]).limit(limit).exec(function(err, scores){
     if(err){
       return next(err);
